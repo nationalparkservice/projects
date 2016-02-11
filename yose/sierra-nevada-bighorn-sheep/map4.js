7a28c6be64e2c9d34589ccf204c5c4112ed73c91
jsoldenwagner <jim_soldenwagner@partner.nps.gov>
2016-02-11 11:10:08 -0800

    further attempt to do the same

--- yose/sierra-nevada-bighorn-sheep/map4.js
+++ yose/sierra-nevada-bighorn-sheep/map4.js
@@ -84,7 +84,7 @@ var NPMap = {
                       result.radius = 5;
                   }
                   
-                  return new L.CircleMarker(latlng, result, fillColor: _assignColor());
+                  return new L.CircleMarker(latlng, result, {fillColor: _assignColor()});
               }
           },
 
